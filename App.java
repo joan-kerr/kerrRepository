5b8b5ce11c550765dc9a6363c810dac2a46b38ee
joan-kerr <60335332+joan-kerr@users.noreply.github.com>
2020-01-31 20:33:32 -0700

    App.java

---
+++ App.java
@@ -0,0 +1,42 @@
+
+public class App {
+
+	public static void main(String[] args) {
+		// week 1 coding assignment
+int itemPrice1= 15;
+int itemPrice2 = 0;
+int moneyInWallet1 = 20;
+int moneyInWallet2 = 0;
+int numberOfFriends1 = 8;
+int numberOfFriends2 = 0;
+int ageA = 40;
+int ageB = 0;
+String firstName1 = "Joan";
+String firstName2 = "Mary";
+String lastName1 = "Kerr";
+String lastName2 = "Lane";
+char middleInitial1 = 'B'; //the middle initials printed out together not on separate lines
+char middleInitial2 = 'N';
+int moneyLeftInWallet = (moneyInWallet1 - itemPrice1);
+float friendsMadePerYear = (ageA / numberOfFriends1);
+String fullName1 = (firstName1  + " " + middleInitial1 + " " + lastName1);
+
+System.out.println(itemPrice1);
+System.out.println(itemPrice2);
+System.out.println(moneyInWallet1);
+System.out.println(moneyInWallet2);
+System.out.println(numberOfFriends1);
+System.out.println(numberOfFriends2);
+System.out.println(ageA);
+System.out.println(ageB);
+System.out.println(firstName1);
+System.out.println(firstName2);
+System.out.println(lastName1);
+System.out.println(lastName2);
+System.out.print(middleInitial1);
+System.out.println(middleInitial2);
+System.out.println(moneyLeftInWallet);
+System.out.println(friendsMadePerYear);
+System.out.println(fullName1);
+	}
+}
